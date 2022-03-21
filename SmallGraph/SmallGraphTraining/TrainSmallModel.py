839fa4c4d6cd4ecc7e472e50338ed3a061c0b07d
StylianosHerodotou <stylianos_herodotou_1999@outlook.com>
2022-03-21 08:48:39 +0200

    Update TrainSmallModel.py

--- SmallGraph/SmallGraphTraining/TrainSmallModel.py
+++ SmallGraph/SmallGraphTraining/TrainSmallModel.py
@@ -3,20 +3,22 @@ from DatasetRepresentation.NetworkXRepresentation.NetworkXGraphProcessing import
 from DatasetRepresentation.Utils import make_networkx_to_pyg_df
 from sklearn.model_selection import train_test_split
 
-from Utilities.InitGlobalVariables import  dir_to_large
+from Utilities.InitGlobalVariables import dir_to_large
 
 from SmallGraph.SmallGraphTraining.HyperParameterTunningSmall import train_and_write_best_model
 from Utilities.HyperParameterTunning import run_hyper_parameter_tuning
 
 
-def generate_and_save_results_for_small_models(networkx_dataset, hyperparameters, model_hyperparameters, tuning_hyperparameters):
+def generate_and_save_results_for_small_models(networkx_dataset, hyperparameters, model_hyperparameters,
+                                               tuning_hyperparameters):
     # create graph that contains node embeddings from the above.
     large_embedings = read_embedings_file(dir_to_large)
 
-    networkx_dataset.add_large_node_embeddings_to_df(large_embedings,large_emb_names=hyperparameters["large_emb_names"])
+    networkx_dataset.add_large_node_embeddings_to_df(large_embedings,
+                                                     large_emb_names=hyperparameters["large_emb_names"])
 
     # turn graphs to pyg
-    pyg_dataset= make_networkx_to_pyg_df(networkx_dataset)
+    pyg_dataset = make_networkx_to_pyg_df(networkx_dataset)
 
     train_df, test_df = train_test_split(pyg_dataset.df, test_size=1 - hyperparameters["train_size"], random_state=42,
                                          shuffle=True, stratify=None)
@@ -34,3 +36,5 @@ def generate_and_save_results_for_small_models(networkx_dataset, hyperparameters
     for time in range(hyperparameters["number_of_overal_models"]):
         best_config = run_hyper_parameter_tuning(hyperparameters, tuning_hyperparameters)
         train_and_write_best_model(best_config, train_set, hyperparameters)
+
+
