f13e348a5db778bf0b55a46d62138ec0418cb195
StylianosHerodotou <stylianos_herodotou_1999@outlook.com>
2022-03-25 19:21:10 +0200

    ss

--- DatasetRepresentation/DataPreprocessing/BERTProcessing.py
+++ DatasetRepresentation/DataPreprocessing/BERTProcessing.py
@@ -1,10 +1,14 @@
 from transformers import RobertaTokenizer
 
-def generate_roberta_embeddings_for_training_set(train_df,
-                                                 article_column_name="article"):
+from DatasetRepresentation.DataPreprocessing.TextProcessing import article_column_name
+
+roberta_column_name="roberta_column_name"
+def generate_roberta_embeddings_for_training_set(train_df,text_name=None):
+    if text_name==None:
+        text_name=article_column_name
     tokenizer = RobertaTokenizer.from_pretrained("roberta-base")
     roberta_input = []
-    for sentense in train_df[article_column_name].values:
+    for sentense in train_df[text_name].values:
         current = tokenizer.encode_plus(
             sentense,  # Sentence to encode.
             add_special_tokens=True,  # Add '[CLS]' and '[SEP]'
@@ -18,5 +22,4 @@ def generate_roberta_embeddings_for_training_set(train_df,
     return roberta_input
 
 def get_roberta_embeddings_for_training_set(train_df,hyperparameters):
-    roberta_column_name= hyperparameters["roberta_column_name"]
     return train_df[roberta_column_name].tolist()
--- DatasetRepresentation/DataPreprocessing/RNNProcessing.py
+++ DatasetRepresentation/DataPreprocessing/RNNProcessing.py
@@ -2,16 +2,18 @@ import torch
 from tensorflow.keras.preprocessing.text import one_hot
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
+
+clean_column_name="clean_column_name"
 def generate_bag_of_words_embeddings_for_training_set(train_df, hyperparameters):
-    clean_column_name = hyperparameters["clean_column_name"]
 
     corpus = list(train_df[clean_column_name])
     onehot_rep = [one_hot(words, hyperparameters["vocab_size"]) for words in corpus]
     embedded_docs = pad_sequences(onehot_rep, padding='pre', maxlen=hyperparameters["maxlen"])
-    article_representation = torch.from_numpy(embedded_docs).long()
-    return article_representation
+    bag_of_words_representation = torch.from_numpy(embedded_docs).long()
+    return bag_of_words_representation
 
 def get_bag_of_words_embeddings_for_training_set(train_df, hyperparameters):
     return generate_bag_of_words_embeddings_for_training_set(train_df, hyperparameters,
                                                  clean_column_name)
+
 
