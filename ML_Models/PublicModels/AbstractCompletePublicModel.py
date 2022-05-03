eb4181bc4fe67ca4c51ace5d69d031ae6f881675
StylianosHerodotou <stylianos_herodotou_1999@outlook.com>
2022-05-03 15:26:26 +0200

    Update AbstractCompletePublicModel.py

--- ML_Models/PublicModels/AbstractCompletePublicModel.py
+++ ML_Models/PublicModels/AbstractCompletePublicModel.py
@@ -10,7 +10,7 @@ class AbstractCompletePublicModel(AbstractCompleteModel, ABC):
 
     def train_step(self, train_dic):
         self.set_model_parameters_to_training_mode()
-        self.optimizer.self.zero_grad_optimizer()
+        self.zero_grad_optimizer()
         output = self.forward(train_dic)
         loss = self.find_loss(output, train_dic)
         self.loss_backward(loss)
