965e6254aee52668fce968e44308661ae70c174a
StylianosHerodotou <stylianos_herodotou_1999@outlook.com>
2022-05-03 10:53:08 +0200

    Update AbstractKFoldTrainer.py

--- Training/KFoldTraining/AbstractKFoldTrainer.py
+++ Training/KFoldTraining/AbstractKFoldTrainer.py
@@ -3,7 +3,7 @@ from sklearn.model_selection import KFold
 from ..AbstractTrainer import AbstractTrainer
 
 
-class AbstractKFoldTrainer(AbstractTrainer,ABC):
+class AbstractKFoldTrainer(AbstractTrainer, ABC):
     def __init__(self, number_of_splits, random_state=42):
         super().__init__()
         self.number_of_splits = number_of_splits
@@ -17,8 +17,8 @@ class AbstractKFoldTrainer(AbstractTrainer,ABC):
     def create_train_eval_data_for_fold(self, fold_data, pre_processed_data):
         pass
 
-
-    def train(self, training_hyperparameters, graph_hyperparameters, model_hyperparameters, data, in_hyper_parameter_search=True):
+    def train(self, training_hyperparameters, graph_hyperparameters, model_hyperparameters, data,
+              in_hyper_parameter_search=True):
         pre_processed_data = self.preprocess_data(data)
         splits = KFold(n_splits=self.number_of_splits, shuffle=True, random_state=self.random_state)
         data_for_each_split = self.create_data_object_for_each_split(splits, pre_processed_data)
@@ -26,6 +26,6 @@ class AbstractKFoldTrainer(AbstractTrainer,ABC):
             train_data, eval_data = self.create_train_eval_data_for_fold(data_for_fold, pre_processed_data)
             model = self.model_function(model_hyperparameters)
             self.set_new_model_parameters(model, training_hyperparameters, graph_hyperparameters, model_hyperparameters,
-                                 data, pre_processed_data, train_data, eval_data)
-            model.train_fold(training_hyperparameters,train_data, eval_data, fold_number=fold_number,
+                                          data, pre_processed_data, train_data, eval_data)
+            model.train_fold(training_hyperparameters, train_data, eval_data, fold_number=fold_number,
                              in_hyper_parameter_search=in_hyper_parameter_search)
