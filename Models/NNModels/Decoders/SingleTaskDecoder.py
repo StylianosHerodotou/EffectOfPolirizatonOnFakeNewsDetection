1472fbea773474d4fd9525448c2550b75c000ab1
StylianosHerodotou <stylianos_herodotou_1999@outlook.com>
2022-05-05 15:31:29 +0200

    Update SingleTaskDecoder.py

--- Models/NNModels/Decoders/SingleTaskDecoder.py
+++ Models/NNModels/Decoders/SingleTaskDecoder.py
@@ -8,6 +8,8 @@ class SingleTaskDecoder(torch.nn.Module):
     def __init__(self, in_channels, pyg_data, classifier_arguments):
         super().__init__()
         self.loss_arguments = None
+        self.classifier_type = classifier_arguments["classifier_type"]
+
         if classifier_arguments["classifier_type"] == "edge":
             self.classifier = EdgePredictionClassifier(in_channels=2* in_channels,
                                                        output_size=classifier_arguments["output_size"],
@@ -20,10 +22,10 @@ class SingleTaskDecoder(torch.nn.Module):
                                                        final_activation_function=classifier_arguments[
                                                            "final_activation_function"]
                                                        )
-            # if "edge_type" in classifier_arguments.keys():
-            #     print("i am here")
-            self.loss_arguments = {"edge_type": classifier_arguments["edge_type"],
-                                   "feature_name": classifier_arguments["feature_name"]}
+            if "edge_type" in classifier_arguments.keys():
+                print("i am here")
+                self.loss_arguments = {"edge_type": classifier_arguments["edge_type"],
+                                       "feature_name": classifier_arguments["feature_name"]}
         else:
             self.classifier = MLP(in_channels=in_channels,
                                   output_size=classifier_arguments["output_size"],
@@ -43,7 +45,7 @@ class SingleTaskDecoder(torch.nn.Module):
         self.metric_weight = classifier_arguments["metric_weight"]
 
     def forward(self, data, encoder_output):
-        if self.loss_arguments is not None:
+        if self.classifier_type == "edge":
             output = self.classifier.forward(encoder_output, data)
         else:
             output = self.classifier.forward(encoder_output)
