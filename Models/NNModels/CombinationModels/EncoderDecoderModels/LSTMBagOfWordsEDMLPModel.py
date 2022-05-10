ad07ae20dff06c2e9dd61ffc4ba0b21840891e4d
StylianosHerodotou <stylianos_herodotou_1999@outlook.com>
2022-05-10 16:50:47 +0200

    Update LSTMBagOfWordsEDMLPModel.py

--- Models/NNModels/CombinationModels/EncoderDecoderModels/LSTMBagOfWordsEDMLPModel.py
+++ Models/NNModels/CombinationModels/EncoderDecoderModels/LSTMBagOfWordsEDMLPModel.py
@@ -13,7 +13,7 @@ class LSTMBagOfWordsEDMLPModel(torch.nn.Module):
                                                 num_layers=encoder_hyperparameters["num_layers"],
                                                 dropout=encoder_hyperparameters["dropout"])
 
-        self.decoder = MLP(in_channels=2*encoder_hyperparameters["heads"] * encoder_hyperparameters["hidden_size"],
+        self.decoder = MLP(in_channels=encoder_hyperparameters["hidden_dim"],
                               output_size=decoder_hyperparameters["output_size"],
                               nodes_per_hidden_layer=decoder_hyperparameters["nodes_per_hidden_layer"],
                               number_of_hidden_layers=decoder_hyperparameters["number_of_hidden_layers"],
