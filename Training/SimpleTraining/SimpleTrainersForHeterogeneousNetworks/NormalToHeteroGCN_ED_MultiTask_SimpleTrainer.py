7c7bda795a2652a93174abb13e00155def5e8e89
StylianosHerodotou <stylianos_herodotou_1999@outlook.com>
2022-05-20 11:31:03 +0200

    Create NormalToHeteroGCN_ED_MultiTask_SimpleTrainer.py

---
+++ Training/SimpleTraining/SimpleTrainersForHeterogeneousNetworks/NormalToHeteroGCN_ED_MultiTask_SimpleTrainer.py
@@ -0,0 +1,14 @@
+from Models.CompleteModels.PublicModels.PublicHeterogeneousGraphModels.MultiTaskDecoderCompleteModels.NormalToHeteroGCN_ED_MultiTask_CompleteModel import NormalToHeteroGCN_ED_MultiTask_CompleteModel
+from Training.SimpleTraining.SimpleTrainersForHeterogeneousNetworks.AbstractSimpleTrainerForHeterogeneousNetwork import \
+    AbstractSimpleTrainerForHeterogeneousNetwork
+
+
+class NormalToHeteroGCN_ED_MultiTask_SimpleTrainer(AbstractSimpleTrainerForHeterogeneousNetwork):
+
+    def __init__(self):
+        super().__init__()
+        self.model_function =NormalToHeteroGCN_ED_MultiTask_CompleteModel
+
+    def set_new_model_parameters(self, model, training_hyperparameters, model_hyperparameters,
+                                 data, pre_processed_data, train_data, eval_data):
+        pass
