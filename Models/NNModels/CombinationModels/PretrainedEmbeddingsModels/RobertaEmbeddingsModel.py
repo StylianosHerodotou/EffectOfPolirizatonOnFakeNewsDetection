2ed64d6fb9fd5420c0574e559247b9d6234dacc4
StylianosHerodotou <stylianos_herodotou_1999@outlook.com>
2022-05-24 17:09:24 +0200

    Update RobertaEmbeddingsModel.py

--- Models/NNModels/CombinationModels/PretrainedEmbeddingsModels/RobertaEmbeddingsModel.py
+++ Models/NNModels/CombinationModels/PretrainedEmbeddingsModels/RobertaEmbeddingsModel.py
@@ -7,17 +7,14 @@ class RobertaEmbeddingsModel(torch.nn.Module):
     def __init__(self, input_hyperparameters, decoder_hyperparameters):
         super().__init__()
         self.decoder = MLP(in_channels=input_hyperparameters["RobertaEmbeddings"]["sizeOfEmbeddings"],
-                              output_size=decoder_hyperparameters["output_size"],
-                              nodes_per_hidden_layer=decoder_hyperparameters["nodes_per_hidden_layer"],
-                              dropout=decoder_hyperparameters["dropout"])
+                           output_size=decoder_hyperparameters["output_size"],
+                           nodes_per_hidden_layer=decoder_hyperparameters["nodes_per_hidden_layer"],
+                           dropout=decoder_hyperparameters["dropout"])
+
     def forward(self, data):
-        HomogeneousGAT_output = self.encoders["HomogeneousGAT"].forward(data)
-
         batch_size = data.batch.max() + 1
-        RobertaEmbeddings = data.extra_inputs[roberta_column_name]
-        RobertaEmbeddings = RobertaEmbeddings.view(batch_size, -1)
+        roberta_embeddings = data.extra_inputs[roberta_column_name]
+        roberta_embeddings = roberta_embeddings.view(batch_size, -1)
 
-        decoder_input =torch.cat((HomogeneousGAT_output, RobertaEmbeddings), -1)
-
-        decoder_output = self.decoder.forward(decoder_input)
-        return decoder_input, decoder_output
+        decoder_output = self.decoder.forward(roberta_embeddings)
+        return decoder_output
