f744afbb9bb21057a167b1832bb5778147f074da
StylianosHerodotou <stylianos_herodotou_1999@outlook.com>
2022-05-26 11:01:06 +0200

    s

---
+++ Training/KFoldTraining/KFoldTrainersForPublicModels/KFoldTrainersForHeterogeneousNetworks/HEAT_ED_MultiTask_KFoldTrainer.py
@@ -0,0 +1,13 @@
+from .AbstractKFoldTrainerForHeterogeneousNetworks import AbstractPublicKFoldTrainerForHeterogeneousNetworks
+from Models.CompleteModels.PublicModels.PublicHeterogeneousGraphModels.MultiTaskDecoderCompleteModels.HEAT_ED_MultiTask_CompleteModel import \
+    HEAT_ED_MultiTask_CompleteModel
+
+class HAN_ED_MultiTask_KFoldTrainer(AbstractPublicKFoldTrainerForHeterogeneousNetworks):
+
+    def __init__(self, number_of_splits, random_state=42):
+        super().__init__(number_of_splits, random_state)
+        self.model_function = HEAT_ED_MultiTask_CompleteModel
+
+    def set_new_model_parameters(self, model, training_hyperparameters, model_hyperparameters,
+                                 data, pre_processed_data, train_data, eval_data):
+        pass
---
+++ Training/SimpleTraining/SimpleTrainersForPublicModels/SimpleTrainersForHeterogeneousNetworks/HEAT_ED_MultiTask_SimpleTrainer.py
@@ -0,0 +1,15 @@
+from Models.CompleteModels.PublicModels.PublicHeterogeneousGraphModels.MultiTaskDecoderCompleteModels.HEAT_ED_MultiTask_CompleteModel import \
+    HEAT_ED_MultiTask_CompleteModel
+from Training.SimpleTraining.SimpleTrainersForPublicModels.SimpleTrainersForHeterogeneousNetworks import \
+    AbstractSimpleTrainerForHeterogeneousNetwork
+
+
+class HEAT_ED_MultiTask_SimpleTrainer(AbstractSimpleTrainerForHeterogeneousNetwork):
+
+    def __init__(self):
+        super().__init__()
+        self.model_function = HEAT_ED_MultiTask_CompleteModel
+
+    def set_new_model_parameters(self, model, training_hyperparameters, model_hyperparameters,
+                                 data, pre_processed_data, train_data, eval_data):
+        pass
