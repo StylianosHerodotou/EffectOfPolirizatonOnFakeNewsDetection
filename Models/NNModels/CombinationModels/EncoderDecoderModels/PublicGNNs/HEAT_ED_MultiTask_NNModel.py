a6ebb298a311f38fe23ec03ab0bc00743c78e0e7
StylianosHerodotou <stylianos_herodotou_1999@outlook.com>
2022-05-26 08:51:10 +0200

    Update HEAT_ED_MultiTask_NNModel.py

--- Models/NNModels/CombinationModels/EncoderDecoderModels/PublicGNNs/HEAT_ED_MultiTask_NNModel.py
+++ Models/NNModels/CombinationModels/EncoderDecoderModels/PublicGNNs/HEAT_ED_MultiTask_NNModel.py
@@ -13,6 +13,7 @@ class HEAT_ED_MultiTask_NNModel(AbstractEncoderDecoderNNModel):
 
         last_layer = encoder_hyperparameters["model_parameters"]["hyper_parameters_for_each_layer"][-1]
         decoder_in_channels = last_layer["hidden_channels"] * last_layer["heads"]
+
         self.decoder = HomogeneousDataMultiTaskDecoder(in_channels=decoder_in_channels,
                                                        pyg_data=decoder_hyperparameters["pyg_data"],
                                                        classifier_per_task_arguments=decoder_hyperparameters[
