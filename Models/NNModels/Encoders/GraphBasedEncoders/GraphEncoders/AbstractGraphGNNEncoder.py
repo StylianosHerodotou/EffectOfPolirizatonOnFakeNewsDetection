a405a3a0589999868816f8b55490147b2e40d9ce
StylianosHerodotou <stylianos_herodotou_1999@outlook.com>
2022-05-27 08:03:13 +0200

    s

--- Models/NNModels/Encoders/GraphBasedEncoders/GraphEncoders/AbstractGraphGNNEncoder.py
+++ Models/NNModels/Encoders/GraphBasedEncoders/GraphEncoders/AbstractGraphGNNEncoder.py
@@ -5,12 +5,19 @@ from Models.NNModels.Encoders.GraphBasedEncoders.AbstractGNNEncoder import Abstr
 
 class AbstractGraphGNNEncoder(AbstractGNNEncoder):
     @abstractmethod
-    def get_pool_layer(self, pyg_data, layer_hyperparameters):
+    def generate_pool_layer(self, pyg_data, layer_hyperparameters):
         pass
 
     @abstractmethod
-    def add_pool_layers(self, in_channels, pyg_data, model_parameters):
+    def generate_hyperparameters_for_each_pool_layer(self, in_channels, pyg_data, model_parameters):
         pass
+
+    def add_pool_layers(self, in_channels, pyg_data, model_parameters):
+        hyperparameters_for_each_layer = self.generate_hyperparameters_for_each_pool_layer(in_channels, pyg_data,
+                                                                                           model_parameters)
+        for layer_hyperparameters in hyperparameters_for_each_layer:
+            new_pool_layer = self.generate_pool_layer(pyg_data, layer_hyperparameters)
+            self.pools.append(new_pool_layer)
 
     def __init__(self, in_channels, pyg_data,model_parameters):
         super().__init__(in_channels, pyg_data,model_parameters)
---
+++ Models/NNModels/Encoders/GraphBasedEncoders/GraphEncoders/HEATGraphEncoder.py
@@ -0,0 +1,111 @@
+from torch_geometric.nn import HEATConv, MemPooling, DeepGCNLayer
+from torch.nn import BatchNorm1d, LeakyReLU, Linear
+import torch.nn.functional as F
+
+from Models.NNModels.Encoders.GraphBasedEncoders.GraphEncoders.AbstractGraphGNNEncoder import AbstractGraphGNNEncoder
+
+
+class HEATGraphEncoder(AbstractGraphGNNEncoder):
+
+    def generate_conv_layer(self, pyg_data, layer_hyperparameters, aggr_type="mean"):
+        conv_layer = HEATConv(in_channels=layer_hyperparameters["in_channels"],
+                              out_channels=layer_hyperparameters["hidden_channels"],
+                              num_node_types=layer_hyperparameters["num_node_types"],
+                              num_edge_types=layer_hyperparameters["num_edge_types"],
+                              edge_type_emb_dim=layer_hyperparameters["edge_type_emb_dim"],
+                              edge_dim=layer_hyperparameters["edge_dim"],
+                              edge_attr_emb_dim=layer_hyperparameters["edge_attr_emb_dim"],
+                              heads=layer_hyperparameters["heads"],
+                              dropout=layer_hyperparameters["dropout"],
+                              )
+        norm_layer = BatchNorm1d(layer_hyperparameters["hidden_channels"])
+        activation_layer = LeakyReLU()
+        complete_layer = DeepGCNLayer(conv_layer, norm_layer, activation_layer,
+                                      block='res+', dropout=layer_hyperparameters["dropout"])
+        return complete_layer
+
+    def generate_hyperparameters_for_each_conv_layer(self, in_channels, pyg_data, model_parameters):
+
+        hyperparameters_for_each_layer = []
+        for current_hyperparameters in model_parameters["hyper_parameters_for_each_layer"]:
+            layer_hyperparameters = dict()
+            if len(hyperparameters_for_each_layer) == 0:
+                layer_hyperparameters["in_channels"] = in_channels
+            else:
+                prev_layer = hyperparameters_for_each_layer[-1]
+                layer_hyperparameters["in_channels"] = prev_layer["hidden_channels"] * prev_layer["heads"]
+            layer_hyperparameters["hidden_channels"] = current_hyperparameters["hidden_channels"]
+            layer_hyperparameters["edge_type_emb_dim"] = current_hyperparameters["edge_type_emb_dim"]
+            layer_hyperparameters["edge_attr_emb_dim"] = current_hyperparameters["edge_attr_emb_dim"]
+            layer_hyperparameters["heads"] = current_hyperparameters["heads"]
+            layer_hyperparameters["dropout"] = current_hyperparameters["dropout"]
+
+            layer_hyperparameters["num_node_types"] = model_parameters["num_node_types"]
+            layer_hyperparameters["num_edge_types"] = model_parameters["num_edge_types"]
+            layer_hyperparameters["edge_dim"] = model_parameters["edge_dim"]
+
+            hyperparameters_for_each_layer.append(layer_hyperparameters)
+        return hyperparameters_for_each_layer
+
+    def generate_pool_layer(self, pyg_data, layer_hyperparameters):
+        pooling_layer = MemPooling(in_channels=layer_hyperparameters["in_channels"],
+                                   out_channels=layer_hyperparameters["hidden_channels"],
+                                   heads=layer_hyperparameters["heads"],
+                                   num_clusters=layer_hyperparameters["num_clusters"])
+        dropout= layer_hyperparameters["dropout"]
+        return pooling_layer,dropout
+
+    def generate_hyperparameters_for_each_pool_layer(self, in_channels, pyg_data, model_parameters):
+        conv_hyperparameters_for_each_layer = self.generate_hyperparameters_for_each_conv_layer(in_channels, pyg_data,
+                                                                                                model_parameters)
+        last_conv_layer_hyperparameters = conv_hyperparameters_for_each_layer[-1]
+
+        hyperparameters_for_each_layer = []
+        for current_hyperparameters in model_parameters["hyper_parameters_for_each_layer"]:
+            layer_hyperparameters = dict()
+
+            if len(hyperparameters_for_each_layer) == 0:
+                layer_hyperparameters["in_channels"] = last_conv_layer_hyperparameters["hidden_channels"] * \
+                                                       last_conv_layer_hyperparameters["heads"]
+            else:
+                prev_layer = hyperparameters_for_each_layer[-1]
+                layer_hyperparameters["in_channels"] = prev_layer["hidden_channels"]
+
+            layer_hyperparameters["hidden_channels"] = current_hyperparameters["hidden_channels"]
+            layer_hyperparameters["heads"] = current_hyperparameters["heads"]
+            layer_hyperparameters["num_clusters"] = current_hyperparameters["num_clusters"]
+
+            layer_hyperparameters["dropout"] = model_parameters["dropout"]
+
+            hyperparameters_for_each_layer.append(layer_hyperparameters)
+        return hyperparameters_for_each_layer
+
+    def __init__(self, in_channels, pyg_data, model_parameters):
+        super().__init__(in_channels, pyg_data, model_parameters)
+
+    def forward(self, pyg_data):
+
+        x, edge_index, edge_attr, node_type, edge_type = pyg_data.x, pyg_data.edge_index, pyg_data.edge_attr, pyg_data.node_type, pyg_data.edge_type
+        for index, conv_layer in enumerate(self.convs):
+            x = conv_layer(x, edge_index, node_type, edge_type, edge_attr)
+
+        kl_loss= 0
+        for dropout, pool_layer in self.pools[:-1]:
+            x, Si =  pool_layer(x) #add the batches later.
+            x = F.leaky_relu(x)
+            x = F.dropout(x, p=self.dropout)
+            kl_loss+=MemPooling.kl_loss(Si)
+
+        x, Si = self.pools[-1](x)  # add the batches later.
+        kl_loss += MemPooling.kl_loss(Si)
+
+        ##TODO CHANGE THE BELOW
+        return (
+            F.log_softmax(x.squeeze(1), dim=-1),
+            kl_loss
+        )
+
+
+
+
+
