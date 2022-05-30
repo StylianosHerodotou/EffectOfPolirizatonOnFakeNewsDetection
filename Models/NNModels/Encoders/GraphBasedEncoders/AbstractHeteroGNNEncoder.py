b82e312e65fee318cebebb4ef8c4916eea0b8231
StylianosHerodotou <stylianos_herodotou_1999@outlook.com>
2022-05-30 16:57:37 +0200

    Update AbstractHeteroGNNEncoder.py

--- Models/NNModels/Encoders/GraphBasedEncoders/AbstractHeteroGNNEncoder.py
+++ Models/NNModels/Encoders/GraphBasedEncoders/AbstractHeteroGNNEncoder.py
@@ -16,7 +16,9 @@ class AbstractHeteroGNNEncoder(AbstractGNNEncoder, ABC):
 
     def activation_forward(self, useful_data):
         x_dict = useful_data.x_dict
+        print(x_dict)
         for key, value in x_dict.items():
             x_dict[key] = self.activation_function(value)
+        print("something else \n\n", x_dict)
         useful_data.x_dict = x_dict
         return useful_data
