0fad9bc64eee67b57f2f7668db349861d3123231
StylianosHerodotou <stylianos_herodotou_1999@outlook.com>
2022-05-30 11:09:33 +0200

    Update HEATGraphEncoder.py

--- Models/NNModels/Encoders/GraphBasedEncoders/GraphEncoders/HomoDataModels/HEATGraphEncoder.py
+++ Models/NNModels/Encoders/GraphBasedEncoders/GraphEncoders/HomoDataModels/HEATGraphEncoder.py
@@ -1,8 +1,6 @@
 from Models.NNModels.Encoders.GraphBasedEncoders.AbstractConvLayers.HeteroWithHomoData.HEATConvolution import HEATConvolution
 from Models.NNModels.Encoders.GraphBasedEncoders.GraphEncoders.AbstractPoolLayer.AtEndPooling.CompleteAtEndPoolingMethods.HomoDataAtEndPoolingMethods.HomoMEMPooling import \
     HomoMEMPooling
-from Models.NNModels.Encoders.GraphBasedEncoders.GraphEncoders.AbstractPoolLayer.SubgraphPooling. \
-    CompleteSubgraphPoolingMethods
 from Models.NNModels.Encoders.GraphBasedEncoders.GraphEncoders.HomoDataModels.AbstractHomogeneousGraphGNNEncoder import \
     AbstractHomogeneousGraphGNNEncoder
 
