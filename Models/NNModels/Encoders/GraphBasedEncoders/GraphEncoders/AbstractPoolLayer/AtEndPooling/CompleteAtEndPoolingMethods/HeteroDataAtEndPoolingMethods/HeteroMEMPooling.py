6546a89a70b4dc385fbd082f9ff0604a5b670d0e
StylianosHerodotou <stylianos_herodotou_1999@outlook.com>
2022-06-03 11:39:08 +0200

    Update HeteroMEMPooling.py

--- Models/NNModels/Encoders/GraphBasedEncoders/GraphEncoders/AbstractPoolLayer/AtEndPooling/CompleteAtEndPoolingMethods/HeteroDataAtEndPoolingMethods/HeteroMEMPooling.py
+++ Models/NNModels/Encoders/GraphBasedEncoders/GraphEncoders/AbstractPoolLayer/AtEndPooling/CompleteAtEndPoolingMethods/HeteroDataAtEndPoolingMethods/HeteroMEMPooling.py
@@ -16,23 +16,23 @@ class HeteroMEMPooling(AbstractHeteroAtEndPooling, AbstractMEMPoolingMethod, ABC
 
     def generate_hyperparameters_for_each_pool_layer(self, in_channels, pyg_data, model_parameters,
                                                      conv_hyperparameters_for_each_layer=None):
-        if conv_hyperparameters_for_each_layer is None:
-            conv_hyperparameters_for_each_layer = self.generate_hyperparameters_for_each_conv_layer(in_channels,
-                                                                                                    pyg_data,
-                                                                                                    model_parameters)
-        last_conv_layer_hyperparameters = conv_hyperparameters_for_each_layer[-1]
-
-        first_layer_input = 0
-        for edge_type, edge_conv_hyperparameters in last_conv_layer_hyperparameters.items():
-            first_layer_input+= edge_conv_hyperparameters["hidden_channels"] * \
-                               edge_conv_hyperparameters["heads"]
+        # if conv_hyperparameters_for_each_layer is None:
+        #     conv_hyperparameters_for_each_layer = self.generate_hyperparameters_for_each_conv_layer(in_channels,
+        #                                                                                             pyg_data,
+        #                                                                                             model_parameters)
+        # last_conv_layer_hyperparameters = conv_hyperparameters_for_each_layer[-1]
+        #
+        # first_layer_input = 0
+        # for edge_type, edge_conv_hyperparameters in last_conv_layer_hyperparameters.items():
+        #     first_layer_input+= edge_conv_hyperparameters["hidden_channels"] * \
+        #                        edge_conv_hyperparameters["heads"]
 
         hyperparameters_for_each_layer = list()
         for current_hyperparameters in model_parameters["pooling_hyper_parameters_for_each_layer"]:
             layer_hyperparameters = dict()
 
             if len(hyperparameters_for_each_layer) == 0:
-                layer_hyperparameters["in_channels"] = first_layer_input
+                layer_hyperparameters["in_channels"] = -1
             else:
                 prev_layer = hyperparameters_for_each_layer[-1]
                 layer_hyperparameters["in_channels"] = prev_layer["hidden_channels"]
